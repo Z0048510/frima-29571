fb0f4456270c4ee35edbe9fa848f84a127d5880e
Z0048510 <z0048510@gmail.com>
2021-01-01 17:25:47 +0900

    ユーザ新規登録機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,20 @@ class User < ApplicationRecord
 
   has_many :products
   has_many :purchases
+
+  #password が英数字で入力されるバリデート
+  validates :password, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*[\d])[a-z\d]+\z/, message: "Include both letters and numbers"}
+  validates :password, presence: true, length: { minimum: 6 }
+
+  #名前 が全角かなで入力されるバリデート
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: "Full-width characters"} do
+    validates :firstname
+    validates :familyname
+  end
+
+  #カナ が全角カナで入力されるバリデート
+  with_options presence: true, format: { with: /\A[ァ-ヶ]+\z/, message: "Full-width katakana characters"} do
+    validates :firstnameasc
+    validates :familynameasc
+  end
 end
