9e723c7152f02c90dc8087053b515df2779298d9
Z0048510 <z0048510@gmail.com>
2021-01-03 23:07:16 +0900

    出品機能(手数料利益計算未搭載)

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,23 @@
 class ItemsController < ApplicationController
   def index
   end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:image, :productname, :amount, :description, :category_id, :status_id, :payment_id, :area_id, :day_id).merge(user_id: current_user.id)
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,23 @@
 class Product < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
   has_one :purchase
+  has_one_attached :image
+  belongs_to :area
+  belongs_to :category
+  belongs_to :payment
+  belongs_to :status
+
+  with_options presence: true do
+    validates :productname
+    validates :description
+    validates :category_id
+    validates :status_id
+    validates :payment_id
+    validates :area_id
+    validates :day_id
+    validates :amount
+  end
+  validates :amount, numericality: {greater_than_or_equal_to: 300, message: "Price Out of setting range"}
+  validates :amount, numericality: {less_than_or_equal_to: 9999999, message: "Price Out of setting range"}
 end
