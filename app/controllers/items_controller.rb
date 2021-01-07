a2cf0678820157139d37aa82cae3d761925ebbd9
Z0048510 <z0048510@gmail.com>
2021-01-07 22:27:06 +0900

    一覧表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,9 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, only: [:new]
+
   def index
+    @products = Product.all.order("created_at DESC")
   end
 
   def new
