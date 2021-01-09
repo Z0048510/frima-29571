d6ec56e19c0aa5314c33e1a8d60dfc8f9de6d907
Z0048510 <z0048510@gmail.com>
2021-01-09 18:46:39 +0900

    レビュー後修正１

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :product_find_initialize, only: [:show, :edit, :update, :destroy]
+  before_action :user_check, only: [:edit, :update, :destroy]
 
   def index
     @products = Product.all.order('created_at DESC')
@@ -23,9 +24,6 @@ class ItemsController < ApplicationController
   end
 
   def edit
-    if current_user.id != @product.user.id
-      redirect_to root_path
-    end
   end
 
   def update
@@ -50,6 +48,12 @@ class ItemsController < ApplicationController
 
   def product_find_initialize
     @product = Product.find(params[:id])
+  end
+
+  def user_check
+    if current_user.id != @product.user.id
+      redirect_to root_path
+    end
   end
 
 end
